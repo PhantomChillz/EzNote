d4cae3000d82f7b57c377feb8c9592c3844cfbdc
Akshayy <Rushiakshay@gmail.com>
2025-02-04 00:16:19 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,7 @@ urlpatterns = [
     path('api-auth/',include("rest_framework.urls")),
     path('api/',include("api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
